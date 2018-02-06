253f9ff0c38e4a68d85ea8135fdd8ad2bbe24e37
psoodan <psoodan@CSCINDAH503788.asiapac.globalcsc.net>
2018-02-07 02:17:37 +0530

    Second Commit

--- src/test/java/Report1/Smartreport3/DownLoadReport3.java
+++ src/test/java/Report1/Smartreport3/DownLoadReport3.java
@@ -35,7 +35,7 @@ public class DownLoadReport3 {
 		        driver.findElement(By.xpath("//*[@id='Menu2']/li[1]/ul/li[1]/a/span[2]")).click();
 		        driver.findElement(By.xpath("//*[@id='Menu2']/li[1]/ul/li[1]/ul/li[1]/a/span")).click();
 		        Select AccountName = new Select(driver.findElement(By.xpath("html/body/table[1]/tbody/tr[3]/td/table/tbody/tr[1]/td[3]/select")));
-		        AccountName.selectByVisibleText("CSC Computer Science Corp (CSC)");
+		        AccountName.selectByVisibleText("Telenor (TLR)");
 		        driver.findElement(By.xpath(".//*[@id='button']")).click();
 		        Select View = new Select(driver.findElement(By.xpath(".//*[@name='reportview']")));
 		        View.selectByVisibleText("Historical Events");
